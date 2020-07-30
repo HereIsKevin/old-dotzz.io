096e821dc600c0be546d5c1e54e8e6d3443d836d
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2020-07-30 15:57:39 -0700

    Write simple but performant router

---
+++ packages/server/src/router.ts
@@ -0,0 +1,61 @@
+export { Route, Router };
+
+import { Handler } from "./server";
+
+import * as http from "http";
+import * as url from "url";
+
+interface Route {
+  children: Record<string, Route>;
+  handler?: Handler;
+}
+
+class Router {
+  private tree: Route;
+
+  public constructor() {
+    this.tree = { children: {} };
+  }
+
+  public route(path: string, handler: Handler) {
+    const parts = path.split("/").filter((x) => x !== "");
+
+    let current = this.tree;
+
+    for (const part of parts) {
+      this.tree.children[part] = { children: {} };
+      current = this.tree.children[part];
+    }
+
+    current.handler = handler;
+  }
+
+  public async handle(
+    request: http.IncomingMessage,
+    response: http.ServerResponse
+  ): Promise<void> {
+    if (typeof request.url === "undefined") {
+      return;
+    }
+
+    const parts = new url.URL(`https://hostname/${request.url}`).pathname
+      .split("/")
+      .filter((x) => x !== "");
+
+    let current = this.tree;
+
+    for (const part of parts) {
+      const next = current.children[part];
+
+      if (typeof next === "undefined") {
+        return;
+      }
+
+      current = next;
+    }
+
+    if (typeof current.handler !== "undefined") {
+      await current.handler(request, response);
+    }
+  }
+}
