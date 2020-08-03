6e4d8ec44ad454516b2b10fd2a3b2c9485a542f5
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2020-08-03 16:35:04 -0700

    Clean up client

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -12,51 +12,12 @@ const game = new Game(target);
 const connection = new WebSocket("ws://localhost:8000/");
 const players: Record<string, Person> = {};
 
-const maxSpeed = 4;
-const acceleraion = 0.4;
-const deceleration = 0.1;
-
 const keys = {
   up: false,
   down: false,
   right: false,
   left: false,
 };
-
-const speed = {
-  y: 0,
-  x: 0,
-};
-
-const player = new Person(0, 0);
-
-connection.addEventListener("message", (event) => {
-  const data = JSON.parse(event.data);
-
-  if (data.kind === "add") {
-    players[data.id] = new Person(data.x, data.y);
-  } else if (data.kind === "move") {
-    const current = players[data.id];
-
-    if (typeof current !== "undefined") {
-      current.x = data.x;
-      current.y = data.y;
-    }
-  } else if (data.kind === "remove") {
-    delete players[data.id];
-  }
-});
-
-let movable = false;
-
-connection.addEventListener("open", (event) => {
-  connection.send(
-    JSON.stringify({ kind: "initialize", x: player.x, y: player.y })
-  );
-  movable = true;
-});
-
-game.sprites.push(player);
 
 game.on("keydown", (event: Event) => {
   switch ((event as KeyboardEvent).key) {
@@ -92,55 +53,41 @@ game.on("keyup", (event: Event) => {
   }
 });
 
-game.tasks.push(() => {
-  player.x += speed.x;
-  player.y += speed.y;
+connection.addEventListener("message", (event) => {
+  const data = JSON.parse(event.data);
 
-  if (keys.down && speed.y < maxSpeed) {
-    speed.y += acceleraion;
-  } else if (keys.up && speed.y > -maxSpeed) {
-    speed.y -= acceleraion;
-  } else if (speed.y < 0) {
-    speed.y += deceleration;
-  } else if (speed.y > 0) {
-    speed.y -= deceleration;
-  }
+  if (data.kind === "add") {
+    players[data.id] = new Person(data.x, data.y);
+  } else if (data.kind === "move") {
+    const current = players[data.id];
 
-  if (keys.right && speed.x < maxSpeed) {
-    speed.x += acceleraion;
-  } else if (keys.left && speed.x > -maxSpeed) {
-    speed.x -= acceleraion;
-  } else if (speed.x < 0) {
-    speed.x += deceleration;
-  } else if (speed.x > 0) {
-    speed.x -= deceleration;
-  }
-
-  if (player.y < 0) {
-    player.y = 0;
-  } else if (player.y > window.innerHeight) {
-    player.y = window.innerHeight;
-  }
-
-  if (player.x < 0) {
-    player.x = 0;
-  } else if (player.x > window.innerWidth) {
-    player.x = window.innerWidth;
-  }
-
-  if (movable) {
-    connection.send(JSON.stringify({ kind: "move", x: player.x, y: player.y }));
+    if (typeof current !== "undefined") {
+      current.x = data.x;
+      current.y = data.y;
+    }
+  } else if (data.kind === "remove") {
+    delete players[data.id];
   }
 });
 
-game.tasks.push(() => {
-  for (const thing of Object.values(players)) {
-    thing.render(game.context);
-  }
+connection.addEventListener("open", (event) => {
+  game.tasks.push(() => {
+    connection.send(
+      JSON.stringify({
+        kind: "move",
+        left: keys.left,
+        right: keys.right,
+        up: keys.up,
+        down: keys.down,
+      })
+    );
+  });
 });
 
-window.addEventListener("beforeunload", () => {
-  connection.send(JSON.stringify({ kind: "remove" }));
+game.tasks.push(() => {
+  for (const player of Object.values(players)) {
+    player.render(game.context);
+  }
 });
 
 game.play();
