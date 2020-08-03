c3ab5e72ff9015a87114aa98146a1acdbbc48e4d
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2020-08-02 18:17:16 -0700

    Improve and clean up server

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,108 +1,118 @@
-import * as http from "http";
-import * as fs from "fs/promises";
+import { v4 as uuidv4 } from "uuid";
 import WebSocket from "ws";
 
 import { Router } from "./router.js";
 import { Server } from "./server.js";
 
-const server = new Server();
-const router = new Router();
-
-const websocket = new WebSocket.Server({ server: server.server });
-
-let id = 0;
-const connections: WebSocket[] = [];
-const players: { [key: number]: { x: number; y: number } } = {};
-
-websocket.on("connection", (socket) => {
-  socket.on("close", () => {});
-
-  socket.on("message", (message) => {
-    const data = JSON.parse(String(message));
-
-    if (data.kind === "request") {
-      connections[id] = socket;
-      id++;
-
-      players[id] = { x: data.x, y: data.y };
-
-      socket.send(JSON.stringify({ kind: "assign", id }));
-
-      const response = JSON.stringify({
-        kind: "add",
-        id,
-        x: data.x,
-        y: data.y,
-      });
-
-      for (const connection of connections) {
-        if (connection !== connections[id]) {
-          connection.send(response);
-        }
-      }
-
-      for (const [id, player] of Object.entries(players)) {
-        socket.send(JSON.stringify({ kind: "add", id, x: player.x, y: player.y }));
-      }
-    } else if (data.kind === "move") {
-      players[data.id] = { x: data.x, y: data.y };
-
-      const response = JSON.stringify({
-        x: data.x,
-        y: data.y,
-        id: data.id,
-        kind: "move",
-      });
-
-      for (const connection of connections) {
-        connection.send(response);
-      }
-    } else if (data.kind === "dead") {
-      const response = JSON.stringify({ kind: "remove", id: data.id });
-
-      for (const connection of connections) {
-        if (connection !== connections[data.id]) {
-          connection.send(response);
-        }
-      }
-
-      delete players[data.id];
-    }
-  });
-});
-
-async function sendFile(
-  request: http.IncomingMessage,
-  response: http.ServerResponse,
-  type: string,
-  path: string
-) {
-  response.setHeader("Content-Type", type);
-  response.writeHead(200);
-  response.end(await fs.readFile(path));
+interface DotZZConfig {
+  port: number;
+  host: string;
 }
 
-router.route("/", async (request, response) =>
-  sendFile(request, response, "text/html", "../client/index.html")
-);
+interface Player {
+  x: number;
+  y: number;
+}
 
-router.route("/build/index.js", async (request, response) =>
-  sendFile(request, response, "text/javascript", "../client/build/index.js")
-);
+class DotZZ {
+  private server: Server;
+  private router: Router;
+  private socket: WebSocket.Server;
+  private config: DotZZConfig;
+  private connectionIds: WeakMap<WebSocket, string>;
+  private players: Record<string, Player>;
 
-router.route("/build/game.js", async (request, response) =>
-  sendFile(request, response, "text/javascript", "../client/build/game.js")
-);
+  public constructor(
+    { port, host }: DotZZConfig = { port: 8000, host: "localhost" }
+  ) {
+    this.server = new Server();
+    this.router = new Router();
+    this.socket = new WebSocket.Server({ server: this.server.server });
+    this.config = { port, host };
+    this.connectionIds = new WeakMap();
+    this.players = {};
 
-router.route("/build/sprites.js", async (request, response) =>
-  sendFile(request, response, "text/javascript", "../client/build/sprites.js")
-);
+    this.routeStatic();
+  }
 
-router.route("/index.css", async (request, response) =>
-  sendFile(request, response, "text/css", "../client/index.css")
-);
+  private routeStatic() {
+    this.router.routeStaticFile("/", "../client/index.html");
+    this.router.routeStaticFile("/index.css", "../client/index.css");
+    this.router.routeStaticDirectory("/build/", "../client/build/");
+  }
 
-server.use(async (request, response) => router.handle(request, response));
-server.use(async (request, response) => response.end("404 Not Found"));
+  public listen() {
+    this.socket.on("connection", (connection, request) => {
+      if (!this.connectionIds.has(connection)) {
+        this.connectionIds.set(connection, uuidv4());
+      }
 
-server.listen(80);
+      connection.on("message", (message) => {
+        const data = JSON.parse(String(message));
+        const connectionId = this.connectionIds.get(connection) as string;
+
+        if (data.kind === "initialize") {
+          console.log("initialize", data);
+
+          for (const [id, location] of Object.entries(this.players)) {
+            connection.send(JSON.stringify({ kind: "add", id, ...location }));
+          }
+
+          this.players[connectionId] = { x: data.x, y: data.y };
+
+          const response = JSON.stringify({
+            kind: "add",
+            id: connectionId,
+            ...this.players[connectionId],
+          });
+
+          for (const client of this.socket.clients.values()) {
+            if (client !== connection) {
+              client.send(response);
+            }
+          }
+        } else if (data.kind === "move") {
+          console.log("move", data);
+
+          this.players[connectionId] = { x: data.x, y: data.y };
+
+          const response = JSON.stringify({
+            kind: "move",
+            id: connectionId,
+            ...this.players[connectionId],
+          });
+
+          for (const client of this.socket.clients.values()) {
+            if (client !== connection) {
+              client.send(response);
+            }
+          }
+        } else if (data.kind === "remove") {
+          console.log("remove", data);
+
+          const response = JSON.stringify({ kind: "remove", id: connectionId });
+
+          for (const client of this.socket.clients.values()) {
+            if (client !== connection) {
+              client.send(response);
+            }
+          }
+
+          delete this.players[connectionId];
+          this.connectionIds.delete(connection);
+        }
+      });
+    });
+
+    this.server.use(async (request, response) => this.router.handle(request, response));
+    this.server.use(async (request, response) => {
+      response.writeHead(404);
+      response.end("404 Not Found");
+    });
+
+    this.server.listen(8000);
+  }
+}
+
+const dotzz = new DotZZ();
+dotzz.listen();
