1da749f02be170a4f4e28549ae6c330657af6270
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2020-08-04 19:42:35 -0700

    Use frame timer in server side arena

--- packages/server/src/arena.ts
+++ packages/server/src/arena.ts
@@ -7,6 +7,7 @@ interface Player {
   y: number;
   velocityX: number;
   velocityY: number;
+  movement: Movement;
 }
 
 interface Position {
@@ -27,6 +28,7 @@ interface ArenaConfig {
   velocityIncrease: number;
   velocityDecrease: number;
   maxVelocity: number;
+  moveInterval: number;
 }
 
 const arenaConfig = {
@@ -35,6 +37,7 @@ const arenaConfig = {
   velocityIncrease: 0.4,
   velocityDecrease: 0.1,
   maxVelocity: 4,
+  moveInterval: 1000 / 60,
 };
 
 class Arena {
@@ -44,12 +47,74 @@ class Arena {
   public constructor(config: ArenaConfig = arenaConfig) {
     this.players = {};
     this.config = { ...arenaConfig, ...config };
+
+    global.setInterval(() => {
+      for (const player of Object.values(this.players)) {
+        // change x coordinate by current x velocity
+        player.x += player.velocityX;
+        // change y coordinate by current y velocity
+        player.y += player.velocityY;
+
+        const maxVelocity = this.config.maxVelocity;
+        const velocityIncrease = this.config.velocityIncrease;
+        const velocityDecrease = this.config.velocityDecrease
+
+        if (player.movement.down && player.velocityY < maxVelocity) {
+          // increase y velocity when possible while moving down
+          player.velocityY += velocityIncrease;
+        } else if (player.movement.up && player.velocityY > -maxVelocity) {
+          // increase y velocity when possible while moving up
+          player.velocityY -= velocityIncrease;
+        } else if (player.velocityY < 0) {
+          // decrease y velocity when not moving up
+          player.velocityY += velocityDecrease;
+        } else if (player.velocityY > 0) {
+          // decrease y velocity when not moving down
+          player.velocityY -= velocityDecrease;
+        }
+
+        if (player.movement.right && player.velocityX < maxVelocity) {
+          // increase x velocity when possible while moving right
+          player.velocityX += velocityIncrease;
+        } else if (player.movement.left && player.velocityX > -maxVelocity) {
+          // increase x velocity when possible while moving left
+          player.velocityX -= velocityIncrease;
+        } else if (player.velocityX < 0) {
+          // decrease x velocity when not moving left
+          player.velocityX += velocityDecrease;
+        } else if (player.velocityX > 0) {
+          // decrease y velocity when not moving right
+          player.velocityX -= velocityDecrease;
+        }
+
+        // make sure y is within boundaries
+        if (player.y < 0) {
+          player.y = 0;
+        } else if (player.y > this.config.height) {
+          player.y = this.config.height;
+        }
+
+        // make sure x is within boundaries
+        if (player.x < 0) {
+          player.x = 0;
+        } else if (player.x > this.config.width) {
+          player.x = this.config.width;
+        }
+      }
+    }, this.config.moveInterval);
   }
 
   public addPlayer(x: number, y: number): string {
     const id = uuidv4();
 
-    this.players[id] = { x, y, velocityX: 0, velocityY: 0 };
+    this.players[id] = {
+      x,
+      y,
+      velocityX: 0,
+      velocityY: 0,
+      movement: { down: false, up: false, right: false, left: false },
+    };
+
     return id;
   }
 
@@ -57,46 +122,9 @@ class Arena {
     delete this.players[id];
   }
 
-  public movePlayer(id: string, directions: Movement): Position {
+  public movePlayer(id: string, movement: Movement): Position {
     const player = this.players[id];
-    const maxVelocity = this.config.maxVelocity;
-    const velocityIncrease = this.config.velocityIncrease;
-    const velocityDecrease = this.config.velocityDecrease;
-
-    player.x += player.velocityX;
-    player.y += player.velocityY;
-
-    if (directions.down && player.velocityY < maxVelocity) {
-      player.velocityY += velocityIncrease;
-    } else if (directions.up && player.velocityY > -maxVelocity) {
-      player.velocityY -= velocityIncrease;
-    } else if (player.velocityY < 0) {
-      player.velocityY += velocityDecrease;
-    } else if (player.velocityY > 0) {
-      player.velocityY -= velocityDecrease;
-    }
-
-    if (directions.right && player.velocityX < maxVelocity) {
-      player.velocityX += velocityIncrease;
-    } else if (directions.left && player.velocityX > -maxVelocity) {
-      player.velocityX -= velocityIncrease;
-    } else if (player.velocityX < 0) {
-      player.velocityX += velocityDecrease;
-    } else if (player.velocityX > 0) {
-      player.velocityX -= velocityDecrease;
-    }
-
-    if (player.y < 0) {
-      player.y = 0;
-    } else if (player.y > this.config.height) {
-      player.y = this.config.height;
-    }
-
-    if (player.x < 0) {
-      player.x = 0;
-    } else if (player.x > this.config.width) {
-      player.x = this.config.width;
-    }
+    player.movement = movement;
 
     return { x: player.x, y: player.y };
   }
