1c179d73430cdb77a1bb8e526a01b90ab4b9aed9
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2020-08-08 09:40:36 -0700

    Create client rollup config

---
+++ packages/client/rollup.config.js
@@ -0,0 +1,14 @@
+import alias from "@rollup/plugin-alias";
+
+export default {
+  input: "./build/index.js",
+  output: {
+    file: "./dist/index.js",
+    format: "esm",
+  },
+  plugins: [
+    alias({
+      entries: [{ find: "@shared", replacement: "../shared/build/" }],
+    }),
+  ],
+};
