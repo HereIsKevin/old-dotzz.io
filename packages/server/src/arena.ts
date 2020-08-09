0ba13a6c48b5c5a36f08350424e98c311ca3dbf8
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2020-08-08 17:41:39 -0700

    Simplify arena code

--- packages/server/src/arena.ts
+++ packages/server/src/arena.ts
@@ -1,60 +1,43 @@
-export { Arena, ArenaConfig, Movement, Player };
+export { Arena };
 
-import { move as movePlayer, restrict as restrictPlayer } from "shared/player";
+// shared configuration
+import { Config, defaultConfig } from "shared/config";
+
+// shared player movement helpers
+import {
+  Movement,
+  Player,
+  move as movePlayer,
+  restrict as restrictPlayer,
+} from "shared/player";
+
+// unique id generator
 import { v4 as uuidv4 } from "uuid";
-
-interface Player {
-  x: number;
-  y: number;
-  velocityX: number;
-  velocityY: number;
-  movement: Movement;
-}
-
-interface Movement {
-  left: boolean;
-  right: boolean;
-  up: boolean;
-  down: boolean;
-}
-
-interface ArenaConfig {
-  width: number;
-  height: number;
-  velocityIncrease: number;
-  velocityDecrease: number;
-  maxVelocity: number;
-  moveInterval: number;
-}
-
-const arenaConfig = {
-  width: 1000,
-  height: 1000,
-  velocityIncrease: 0.4,
-  velocityDecrease: 0.1,
-  maxVelocity: 4,
-  moveInterval: 1000 / 60,
-};
 
 class Arena {
   public players: Record<string, Player>;
-  public config: ArenaConfig;
+  public config: Config;
 
-  public constructor(config: ArenaConfig = arenaConfig) {
+  public constructor(config: Config = defaultConfig) {
     this.players = {};
-    this.config = { ...arenaConfig, ...config };
+    this.config = { ...defaultConfig, ...config };
 
+    // move all players every frame
     global.setInterval(() => {
       for (const player of Object.values(this.players)) {
+        // move the player based on velocity and directions
         movePlayer(player);
+        // make sure the player is restricted in arena
         restrictPlayer(player);
       }
-    }, this.config.moveInterval);
+    }, this.config.frameRate);
   }
 
   public addPlayer(x: number, y: number): string {
+    // generate unique id for new player
     const id = uuidv4();
 
+    // create new empty player
     this.players[id] = {
       x,
       y,
@@ -67,13 +50,12 @@ class Arena {
   }
 
   public removePlayer(id: string): void {
+    // permanently remove player
     delete this.players[id];
   }
 
-  public movePlayer(id: string, movement: Movement): Player {
-    const player = this.players[id];
-    player.movement = movement;
-
-    return player;
+  public movePlayer(id: string, movement: Movement): void {
+    // update player movement directions
+    this.players[id].movement = movement;
   }
 }
