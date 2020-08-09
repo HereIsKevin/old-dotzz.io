5a309f0d976e2dc0b376ce306299c9bb1e071450
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2020-08-08 18:11:41 -0700

    Fully comment server

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -15,16 +15,22 @@ class Server {
     this.server = new http.Server();
     this.handlers = [];
 
-    this.server.on("request", this.handle.bind(this));
+    // add event listener for server request
+    this.server.on("request", (request, response) =>
+      this.handle(request, response)
+    );
   }
 
   private async handle(
     request: http.IncomingMessage,
     response: http.ServerResponse
   ): Promise<void> {
+    // go through all request handlers
     for (const handler of this.handlers) {
+      // wait for the handler to finish
       await handler(request, response);
 
+      // stop if the handler succeeded
       if (response.writableEnded) {
         break;
       }
@@ -32,10 +38,12 @@ class Server {
   }
 
   public use(handler: Handler): void {
+    // add new handlers sequentially
     this.handlers.push(handler);
   }
 
   public listen(port: number, host: string = "localhost"): void {
+    // run the server event loop
     this.server.listen(port, host);
   }
 }
