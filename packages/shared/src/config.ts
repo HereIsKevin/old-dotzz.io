a1359827be55ced5c2d6bc2ae513d867fcecfcdd
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2020-08-11 17:10:57 -0700

    Use more reasonable arena sizes

--- packages/shared/src/config.ts
+++ packages/shared/src/config.ts
@@ -17,13 +17,14 @@ interface Config {
   foodSize: number;
   minSize: number;
   maxSize: number;
+  growth: number;
 }
 
 const defaultConfig: Config = {
   host: "192.168.1.196",
   port: 8000,
-  width: 1000,
-  height: 1000,
+  width: 2000,
+  height: 2000,
   velocityIncrease: 0.4,
   velocityDecrease: 0.1,
   maxVelocity: 4,
@@ -31,9 +32,10 @@ const defaultConfig: Config = {
   responseRate: 1000 / 6,
   tolerance: 1,
   resolve: 2,
-  maxFood: 30,
+  maxFood: 200,
   foodIncrease: 10,
   foodSize: 10,
   minSize: 20,
   maxSize: 40,
+  growth: 0.05,
 };
