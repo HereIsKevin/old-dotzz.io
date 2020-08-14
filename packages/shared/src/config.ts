1458d662c5d9efc57131df261ed0b9d54526e614
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2020-08-14 12:30:08 -0700

    Create a improved config

--- packages/shared/src/config.ts
+++ packages/shared/src/config.ts
@@ -1,41 +1,57 @@
 export { Config, defaultConfig };
 
+interface Velocity {
+  increase: number;
+  decrease: number;
+  max: number;
+}
+
 interface Config {
+  // server
   host: string;
   port: number;
+  // arena
   width: number;
   height: number;
-  velocityIncrease: number;
-  velocityDecrease: number;
-  maxVelocity: number;
+  food: number;
+  foodSize: number;
+  // velocity
+  velocity: Velocity;
+  // intervals
   frameRate: number;
   responseRate: number;
+  // reconcilation
   tolerance: number;
   resolve: number;
-  maxFood: number;
-  foodIncrease: number;
-  foodSize: number;
-  minSize: number;
-  maxSize: number;
+  // player size
+  baseSize: number;
+  sizeChange: 20;
   growth: number;
 }
 
 const defaultConfig: Config = {
+  // server
   host: "192.168.1.196",
   port: 8000,
+  // arena
   width: 2000,
   height: 2000,
-  velocityIncrease: 0.4,
-  velocityDecrease: 0.1,
-  maxVelocity: 4,
+  food: 200,
+  foodSize: 10,
+  // velocity
+  velocity: {
+    increase: 0.4,
+    decrease: 0.1,
+    max: 4,
+  },
+  // intervals
   frameRate: 1000 / 60,
   responseRate: 1000 / 30,
+  // reconcilation
   tolerance: 1,
   resolve: 2,
-  maxFood: 200,
-  foodIncrease: 10,
-  foodSize: 10,
-  minSize: 20,
-  maxSize: 40,
+  // player
+  baseSize: 20,
+  sizeChange: 20,
   growth: 0.05,
 };
