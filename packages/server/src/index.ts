db7959bc3c59825d3f43c8d63998b5fcf859acf8
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2020-08-14 12:30:19 -0700

    Integrate new features into server

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -2,7 +2,7 @@ import { Config, defaultConfig } from "shared/config";
 import { move as movePlayer, restrict as restrictPlayer } from "shared/player";
 import { Arena } from "server/arena";
 import { circleCollided, randint } from "server/utilities";
-import { Movement } from "shared/player";
+import { Movement, grow } from "shared/player";
 import { Router } from "server/router";
 import { Server } from "server/server";
 
@@ -34,6 +34,7 @@ class DotZZ {
 
     // execute tasks at every frame
     global.setInterval(() => {
+      // check all food
       this.eatFood();
 
       for (const player of Object.values(this.arena.players)) {
@@ -43,54 +44,61 @@ class DotZZ {
         restrictPlayer(player);
       }
 
-      const totalFood = Object.keys(this.arena.food).length;
-
-      if (totalFood + this.config.foodIncrease < this.config.maxFood) {
-        this.generateFood(this.config.foodIncrease);
-      } else if (this.config.maxFood - totalFood > 0) {
-        this.generateFood(this.config.maxFood - totalFood);
-      }
+      // generate as much food as needed
+      this.generateFood();
     }, this.config.frameRate);
 
     // route all static resources
     this.routeStatic();
   }
 
-  private generateFood(amount: number): void {
+  private generateFood(): void {
+    // find the difference between the maximum amount of food and current food
+    const amount = this.config.food - Object.keys(this.arena.food).length;
+
     for (let index = 0; index < amount; index++) {
+      // randomly generate location for food
       const x = randint(0, this.config.width);
       const y = randint(0, this.config.height);
+      // create food in arena
       const id = this.arena.createFood(x, y);
 
+      // send message to add food to all clients
       this.sendToAll(JSON.stringify({ kind: "addFood", x, y, id }));
     }
   }
 
   private eatFood(): void {
+    // iterate through all players
     for (const [playerId, player] of Object.entries(this.arena.players)) {
+      // iterate through all food
       for (const [foodId, food] of Object.entries(this.arena.food)) {
+        // check for collision between food and player
         const collided = circleCollided(
           { x: player.x, y: player.y, radius: player.size },
           { x: food.x, y: food.y, radius: this.config.foodSize }
         );
 
         if (collided) {
+          // remove the food on a collision
           this.arena.removeFood(foodId);
 
-          player.size = Math.min(
-            this.config.maxSize,
-            player.size + this.config.growth
-          );
+          // increment the player size by growth if not at max
+          grow(player, this.config.growth);
 
+          // increment the player score
           player.score++;
 
+          // remove the food from all clients
           this.sendToAll(JSON.stringify({ kind: "removeFood", id: foodId }));
+          // resize the current player on all clients
           this.sendToAll(
-            JSON.stringify({ kind: "resize", id: playerId, size: player.size })
+            JSON.stringify({ kind: "resizePlayer", id: playerId, size: player.size })
           );
+          // increment the current player score on all clients
           this.sendToAll(
             JSON.stringify({
-              kind: "scoreUp",
+              kind: "scorePlayer",
               id: playerId,
               score: player.score,
             })
@@ -126,30 +134,32 @@ class DotZZ {
       // make sure the player is not the current connection
       if (id !== connectionId) {
         // send the player data to the connection
-        connection.send(JSON.stringify({ kind: "add", id, ...data }));
+        connection.send(JSON.stringify({ kind: "addPlayer", id, ...data }));
       }
     }
 
+    // iterate through all food
     for (const [id, food] of Object.entries(this.arena.food)) {
+      // send the food data to the connection
       connection.send(
-        JSON.stringify({ kind: "addFood", id, x: food.x, y: food.y })
+        JSON.stringify({  kind: "addFood", id, x: food.x, y: food.y })
       );
     }
 
     // send new player to all existing connections
     this.sendToAll(
       JSON.stringify({
-        kind: "add",
+        kind: "addPlayer",
         id: connectionId,
         x,
         y,
-        size: this.config.minSize,
+        size: this.config.baseSize,
         score: 0,
       })
     );
 
     // provide the new connection with its id
-    connection.send(JSON.stringify({ kind: "id", id: connectionId }));
+    connection.send(JSON.stringify({ kind: "initialize", id: connectionId }));
   }
 
   private movePlayer(connection: WebSocket, message: Movement): void {
@@ -167,7 +177,7 @@ class DotZZ {
     // update player location on all clients
     this.sendToAll(
       JSON.stringify({
-        kind: "move",
+        kind: "movePlayer",
         id: connectionId,
         ...this.arena.players[connectionId],
       })
@@ -184,7 +194,7 @@ class DotZZ {
     }
 
     // send id of the removed player to all clients
-    this.sendToAll(JSON.stringify({ kind: "remove", id: connectionId }));
+    this.sendToAll(JSON.stringify({ kind: "removePlayer", id: connectionId }));
     // remove the player from the arena
     this.arena.removePlayer(connectionId);
     // remove the connection from the WeakMap
@@ -206,7 +216,7 @@ class DotZZ {
       connection.on("message", (message) => {
         const data = JSON.parse(String(message));
 
-        if (data.kind === "move") {
+        if (data.kind === "movePlayer") {
           // move player on a move message
           this.movePlayer(connection, {
             left: data.left,
