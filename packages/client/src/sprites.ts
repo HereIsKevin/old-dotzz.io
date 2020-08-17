57ab52b65899e8118effdc49507228cf52c0b16e
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2020-08-16 17:17:23 -0700

    Clean up sprites

--- packages/client/src/sprites.ts
+++ packages/client/src/sprites.ts
@@ -1,6 +1,7 @@
-export { BorderSprite, Sprite, PlayerSprite, FoodSprite };
+export { BorderSprite, FoodSprite, PlayerSprite, Sprite };
 
-import { Config, defaultConfig } from "shared/config";
+import { defaultConfig as config } from "shared/config";
+import { massToSize } from "shared/sprites";
 
 function circle(
   context: CanvasRenderingContext2D,
@@ -20,75 +21,75 @@ class Sprite {
     this.y = y;
   }
 
-  /* eslint-disable @typescript-eslint/no-unused-vars */
-
   public render(context: CanvasRenderingContext2D): void {
     throw new Error("render is not implemented");
   }
-
-  /* eslint-enable @typescript-eslint/no-unused-vars */
 }
 
 class FoodSprite extends Sprite {
-  public config: Config;
-
-  public constructor(x: number, y: number, config: Config = defaultConfig) {
+  public constructor(x: number, y: number) {
     super(x, y);
-
-    this.config = { ...defaultConfig, ...config };
   }
 
   public render(context: CanvasRenderingContext2D): void {
     context.fillStyle = "rgb(46, 204, 64)";
 
     context.beginPath();
-    circle(context, this.x, this.y, this.config.foodSize);
+    circle(context, this.x, this.y, massToSize(config.foodMass));
     context.fill();
   }
 }
 
 class PlayerSprite extends Sprite {
-  public current: boolean;
   public size: number;
   public score: number;
+  public current: boolean;
 
-  public constructor(x: number, y: number, size: number, score: number) {
+  public constructor(
+    x: number,
+    y: number,
+    size: number,
+    score: number,
+    current: boolean
+  ) {
     super(x, y);
 
     this.size = size;
     this.score = score;
-    this.current = false;
+    this.current = current;
   }
 
   public render(context: CanvasRenderingContext2D): void {
     context.fillStyle = this.current ? "rgb(0, 116, 217)" : "rgb(255, 65, 54)";
 
     context.beginPath();
-    circle(context, this.x, this.y, this.size);
+    circle(context, this.x, this.y, massToSize(this.size));
     context.fill();
 
     context.textAlign = "center";
-    context.fillText(String(this.score) , this.x, this.y - (this.size + 5));
+    context.font = "12px sans-serif";
+    context.fillText(
+      String(this.score),
+      this.x,
+      this.y - (massToSize(this.size) + 15)
+    );
   }
 }
 
 class BorderSprite extends Sprite {
   public direction: string;
   public target: HTMLCanvasElement;
-  public config: Config;
 
   public constructor(
     x: number,
     y: number,
     target: HTMLCanvasElement,
-    direction: string,
-    config: Config = defaultConfig
+    direction: string
   ) {
     super(x, y);
 
     this.target = target;
     this.direction = direction;
-    this.config = { ...defaultConfig, ...config };
   }
 
   public render(context: CanvasRenderingContext2D): void {
@@ -99,27 +100,27 @@ class BorderSprite extends Sprite {
         this.x - this.target.width,
         this.y - this.target.height,
         this.target.width,
-        this.config.height + this.target.height * 2
+        config.height + this.target.height * 2
       );
     } else if (this.direction === "right") {
       context.fillRect(
         this.x,
         this.y - this.target.height,
         this.target.width,
-        this.config.height + this.target.height * 2
+        config.height + this.target.height * 2
       );
     } else if (this.direction === "up") {
       context.fillRect(
         this.x - this.target.width,
         this.y - this.target.height,
-        this.config.width + this.target.width * 2,
+        config.width + this.target.width * 2,
         this.target.height
       );
     } else if (this.direction === "down") {
       context.fillRect(
         this.x - this.target.width,
         this.y,
-        this.config.width + this.target.width * 2,
+        config.width + this.target.width * 2,
         this.target.height
       );
     }
